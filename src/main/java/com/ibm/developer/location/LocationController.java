6d3ade3f7698381bb348237b15c01fa80af4bbf1
Billy Korando <wkorando@gmail.com>
2019-05-09 23:03:41 -0500

    Adding logging message

--- src/main/java/com/ibm/developer/location/LocationController.java
+++ src/main/java/com/ibm/developer/location/LocationController.java
@@ -1,5 +1,7 @@
 package com.ibm.developer.location;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/locations")
 public class LocationController {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
+	
 	@GetMapping
 	public ResponseEntity<String> allLocations(){
+		LOGGER.debug("In allLocations");
 		return ResponseEntity.ok("Hello World");
 	}
 }
